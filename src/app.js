7acb54a5e0f499aedcd76945c6a79bf4bc020969
Spyros Argalias <sargalias@gmail.com>
2023-08-16 00:17:25 +0100

    add swagger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import { initialize } from 'express-openapi';
+import v1WorldsService from './api-v1/services/worldsService.js';
+import v1ApiDoc from './api-v1/api-doc.js';
 
 const app = express();
 const PORT = 3000;
+
+initialize({
+  app,
+  // NOTE: If using yaml you can provide a path relative to process.cwd() e.g.
+  // apiDoc: './api-v1/api-doc.yml',
+  apiDoc: v1ApiDoc,
+  dependencies: {
+    worldsService: v1WorldsService,
+  },
+  paths: './src/api-v1/paths',
+});
 
 app.listen(PORT, (error) => {
   if (!error)
